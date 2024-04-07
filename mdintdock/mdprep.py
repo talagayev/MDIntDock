0d1f0edac6a03e653a57a6633c1b3a9a3b91c4c2
Valerij Talagayev <82884038+talagayev@users.noreply.github.com>
2024-04-08 00:55:36 +0200

    Update mdprep.py

--- mdintdock/mdprep.py
+++ mdintdock/mdprep.py
@@ -4,44 +4,36 @@ from rdkit import Chem
 from rdkit.Chem import SDWriter
 
 class FrameWriter:
-    def __init__(self, topology_file, trajectory_file, lig_resname, output_directory=None):
+    def __init__(self, topology_file, trajectory_file, lig_resname):
         self.topology_file = topology_file
         self.trajectory_file = trajectory_file
         self.lig_resname = lig_resname
+        self.protein_pdb_directory = "protein_frames"
+        self.refligand_sdf_directory = "ligand_frames"
 
-        if output_directory is None:
-            # Extract filename without extension from topology_file
-            folder_name = os.path.splitext(os.path.basename(topology_file))[0]
-            self.output_directory = os.path.join(folder_name + f"_{lig_resname}")
-        else:
-            self.output_directory = output_directory
+        # Create directories if they don't exist
+        os.makedirs(self.protein_pdb_directory, exist_ok=True)
+        os.makedirs(self.refligand_sdf_directory, exist_ok=True)
 
-        # Create output directory if it doesn't exist
-        if not os.path.exists(self.output_directory):
-            os.makedirs(self.output_directory)
-
-        # Create subdirectories
-        self.protein_pdb_directory = os.path.join(self.output_directory, "frames_protein_pdb")
-        self.refligand_sdf_directory = os.path.join(self.output_directory, "frames_refligand_sdf")
-
-        if not os.path.exists(self.protein_pdb_directory):
-            os.makedirs(self.protein_pdb_directory)
-        if not os.path.exists(self.refligand_sdf_directory):
-            os.makedirs(self.refligand_sdf_directory)
-
-    def write_protein_frame(self, frame_number):
+    def write_frames(self):
         u = mda.Universe(self.topology_file, self.trajectory_file)
-        protein = u.select_atoms("protein")
-        frame = protein.positions
-        with mda.Writer(os.path.join(self.protein_pdb_directory, f"frame_{frame_number}.pdb"), protein.n_atoms) as pdb:
-            for atom in protein.atoms:
-                atom.position = frame[atom.index]
-            pdb.write(protein)
 
-    def write_ligand_frame(self, frame_number):
-        u = mda.Universe(self.topology_file, self.trajectory_file)
-        ligand = u.select_atoms(f"resname {self.lig_resname}")
-        mol = ligand.atoms.convert_to("RDKIT")
-        with Chem.SDWriter(os.path.join(self.refligand_sdf_directory, f'ligand_{frame_number}.sdf')) as w:
-            w.write(mol)
+        # Create output directories if they don't exist
+        os.makedirs(self.protein_pdb_directory, exist_ok=True)
+        os.makedirs(self.refligand_sdf_directory, exist_ok=True)
+
+        # Loop through each frame in the trajectory
+        for ts in u.trajectory:
+            frame_number = ts.frame + 1  # Frame number starts from 1
+
+            # Write protein frame
+            protein = u.select_atoms(f"(protein and around 10.0 resname {self.lig_resname})")
+            with mda.Writer(os.path.join(self.protein_pdb_directory, f"frame_{frame_number}.pdb"), protein.n_atoms) as pdb:
+                pdb.write(protein)
+
+            # Write ligand frame
+            ligand = u.select_atoms(f"resname {self.lig_resname}")
+            mol = ligand.atoms.convert_to("RDKIT")
+            with Chem.SDWriter(os.path.join(self.refligand_sdf_directory, f'ligand_{frame_number}.sdf')) as w:
+                w.write(mol)
 
